d162107d192526506b2c699a0ba59ad4313658a4
bekahsealey <bekah@bekahsealey.com>
2015-08-04 15:43:36 -0500

    remove developer links in footer, update p margin and screenshot size

--- footer.php
+++ footer.php
@@ -4,8 +4,6 @@
 	</div>
 	<footer id="credits">
 		<?php theme_credits(); ?>
-		<a href="http://jigsaw.w3.org/css-validator/check/referer/?">CSS</a>
-		<a href="http://validator.w3.org/check?uri=referer">HTML</a>
 	</footer>
 	<?php wp_footer(); ?>
 </body>
